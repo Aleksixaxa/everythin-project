41512707d4f14ce4d831241e2b945ec2f02770c7
Aleksixaxa <55782606+Aleksixaxa@users.noreply.github.com>
2020-02-06 19:48:05 +0200

    weekend done

--- scripts/after-app.js
+++ scripts/after-app.js
@@ -3,6 +3,7 @@ const currentTime = document.getElementById('current-time');
 const currentDate = document.getElementById('current-date');
 const currentDay = document.getElementById('current-day');
 const currentTemperature = document.getElementById('current-temperature');
+const examDay = document.getElementById('exam-day')
 
 const timeApi =`http://worldtimeapi.org/api/timezone/Europe/Helsinki`;
 const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=%C3%A4%C3%A4nekoski&APPID=071656ebca0dd91cb744b42919983bb5`
@@ -12,30 +13,40 @@ let intervalTime = setInterval(time, 1000);
 function time() {
     //Time
     let date = new Date();
-    let time = `${date.getHours()}:${date.getMinutes()}`
-  
+    let time = `${date.getHours()}:${date.getMinutes()}`;
+    let untilWeekend = document.querySelector('#weekend-day');
+
     newDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
     currentDate.innerHTML = newDate
     currentTime.innerHTML = time;
     
     if (date.getDay() == 2) {
         currentDay.innerHTML = 'Tuesday';
+        untilWeekend.innerHTML = 3; // days until weekend
     } else if (date.getDay() == 3) {
         currentDay.innerHTML = 'Wednesday';
+        untilWeekend.innerHTML = 2; // days until weekend
     } else if (date.getDay() == 4) {
         currentDay.innerHTML = 'Thursday';
+        untilWeekend.innerHTML = 1; // days until weekend
     } else if (date.getDay() == 5) {
         currentDay.innerHTML = 'Friday';
+        untilWeekend.innerHTML = 'zeroo';// days until weekend
     } else if (date.getDay() == 6) {
         currentDay.innerHTML = 'Saturday';
+        untilWeekend.innerHTML = 'zerooo';// days until weekend
     } else if (date.getDay() == 7) {
         currentDay.innerHTML = 'Sunday';
+        untilWeekend.innerHTML = 'zeeeerro';// days until weekend
     } else if (date.getDay() == 1) {
         currentDay.innerHTML = 'Monday';
+        untilWeekend.innerHTML = '4...';// days until weekend
     }
-    // if (date.getMinutes() == 1 || 2 || 3 || 4 || 5 || 6) {
-    //     '0' + date.getMinutes().slice(-2);
-    // };
+    // ?clock shows xx.1 not xx.01
+//     if (date.getMinutes() == 4) {
+//         '0' + date.getMinutes()
+//     };
+//     console.log(date.getMinutes())
 };
 
 
@@ -49,8 +60,33 @@ fetch(weatherApi)
     const temperature = response.main.temp - 273.15;
 
     currentTemperature.innerHTML = temperature.toFixed(1) + '°C';
+});
+// *Next Exam =>
+//starting the whole shit
+setInterval( () => {
+    let countDownDate  = new Date("Feb 12, 2020").getTime();
+    let now = new Date().getTime();
+    let untilExam = countDownDate - now
+    let days = Math.floor(untilExam / (1000 * 60 * 60 * 24));
+    let nextExam = document.querySelector('#next-exam');
+    //displaying days until 
+    examDay.innerHTML = days + 1;
+   
+    if (days == -1) {
+        nextExam.innerHTML = 'You have an exam today!';
+    } else if(days == 0 ) {
+        nextExam.innerHTML = 'You have an exam tomorrow!';
+    } else if (days <= -2) {
+        nextExam.innerHTML = 'Change another exam deadline';
+    };
+}, 1000);//1sec refresh on time
 
+setInterval( () => {
+    let countDownDate = new Date("feb 12, 2020").getTime();
+    let now = new Date().getTime();
 
-})
+    console.log(countDownDate)
+}, 1000);
+
 
 
